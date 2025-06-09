c0577a261c817ca2439696969cc1783f056b6695
Léonard Dinichert <84095132+LeonardDinichert@users.noreply.github.com>
2025-06-09 16:13:40 +0200

    Fix group paths and add shared scheme

---
+++ Tests/SchoolAssisstantPackageTests/PlaceholderTests.swift
@@ -0,0 +1,9 @@
+import XCTest
+@testable import SchoolAssisstantPackage
+
+final class PlaceholderTests: XCTestCase {
+    func testHello() {
+        let placeholder = Placeholder()
+        placeholder.hello()
+    }
+}
